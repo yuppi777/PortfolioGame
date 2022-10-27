675fc893bd698129dbede9d1c6a06439323087d4
yuppi777 <88840972+yuppi777@users.noreply.github.com>
2022-10-28 02:52:13 +0900

    ShaderGlaph

---
+++ Assets/Yuppi/Scripts/PlayerCore.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCore : MonoBehaviour
+{
+    [SerializeField]
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
