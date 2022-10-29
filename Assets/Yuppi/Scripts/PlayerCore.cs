f1ea1676e83bfc0b364757bb5dd213f607bb1a97
yuppi777 <88840972+yuppi777@users.noreply.github.com>
2022-10-29 23:18:34 +0900

    PlayerMoverとParamatorの設置

--- Assets/Yuppi/Scripts/PlayerCore.cs
+++ Assets/Yuppi/Scripts/PlayerCore.cs
@@ -7,5 +7,11 @@ public class PlayerCore : MonoBehaviour
     [SerializeField]
     [Header("プレイヤーの動きのスクリプト")]
     private PlayerMover playerMover;
-    
+
+    private PlayerParametor playerParametor;
+
+    private void Update()
+    {
+        playerMover.PlayerMove(playerParametor.PlayerMoveSpeed);
+    }
 }
--- Assets/Yuppi/Scripts/PlayerMover.cs
+++ Assets/Yuppi/Scripts/PlayerMover.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class PlayerMover : MonoBehaviour
 {
- 
+    Rigidbody rb;
+    float x;
+    float z;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+    public void PlayerMove(float moveSpeed)
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        z = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector3(x, 0, z) * moveSpeed;
+    }
+    
 }
